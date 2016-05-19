3c6871ac3c3bb1163b12bdd1dee11e4d7b1ea534
kimhyungjun <hyungjoon6876@gmail.com>
2016-05-19 23:17:15 +0900

    Add daily log file

--- logs/logger.js
+++ logs/logger.js
@@ -1,4 +1,5 @@
 var winston = require('winston');
+
 var logger = new winston.Logger({
     transports : [
       new (winston.transports.Console)({
@@ -7,11 +8,30 @@ var logger = new winston.Logger({
       new (winston.transports.File)({
         level : 'debug',
         json : false,
-        filename : 'log-',
         timestamp : true,
-        datePattern : 'yyyy-MM-dd.log'
+        filename : 'logs-' + getDay() + '.log',
+        dirname : __dirname
       })
     ]
 });
+
+function getDay(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd < 10) {
+      dd = '0' + dd;
+  }
+
+  if(mm < 10) {
+      mm = '0' + mm;
+  }
+  today = yyyy + '-' + mm + '-' + dd;
+
+  return today
+
+}
 
 module.exports = logger;
