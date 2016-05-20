39eae3f506ae476d1d66208404d36eedd835b477
eockim <hyungjoon6876@gmail.com>
2016-05-20 10:24:22 +0900

    fix logger.js

--- logs/logger.js
+++ logs/logger.js
@@ -1,14 +1,5 @@
 var winston = require('winston');
-<<<<<<< HEAD
-var options = {
-  datePattern : 'yyyy-MM-dd',
-  filename : 'log-'
-};
 
-winston.add(require('winston-daily-rotate-file'), options);
-=======
-
->>>>>>> 3c6871ac3c3bb1163b12bdd1dee11e4d7b1ea534
 var logger = new winston.Logger({
     transports : [
       new (winston.transports.Console)({
@@ -17,13 +8,9 @@ var logger = new winston.Logger({
       new (winston.transports.File)({
         level : 'debug',
         json : false,
-<<<<<<< HEAD
-        timestamp : true
-=======
         timestamp : true,
         filename : 'logs-' + getDay() + '.log',
         dirname : __dirname
->>>>>>> 3c6871ac3c3bb1163b12bdd1dee11e4d7b1ea534
       })
     ]
 });
